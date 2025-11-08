45bce69e593dd1bb02d0d4d19a1861d5c903d6e7
AndreBote <andrebote2005@gmail.com>
2025-11-07 19:48:16 -0500

    Update config.py

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "rice_and_shineDB"
     db_port = 3306
     db_user = "root"
-    db_password = "mywashington2016"
+    db_password = "rootroot"
     app_host = "localhost"
     app_port = 8000
