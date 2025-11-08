9d97f24aad8632e9d76fdafc6b2653f72c24742b
“Justin” <jlee356@uncc.edugit config --global user.email jlee356@uncc.edu>
2025-11-07 19:17:06 -0500

    create modes/dishes.py

---
+++ FinalProject/api/models/dishes.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+from ..dependencies.database import Base
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    dish_id = Column(Integer, ForeignKey("dishes.id"))      
+    resource_id = Column(Integer, ForeignKey("resources.id"))
+    amount = Column(Integer, index=True, nullable=False, server_default="0")
+
+    dish = relationship("Dish", back_populates="recipes")     
+    resource = relationship("Resource", back_populates="recipes")
