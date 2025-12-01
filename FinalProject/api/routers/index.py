276326ae182e6c0687eca1ae94aa0e6adac459fc
AndreBote <andrebote2005@gmail.com>
2025-12-01 16:53:22 -0500

    Created dishes.py

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,7 +1,8 @@
-from . import orders, order_details, promotions
+from . import orders, order_details, dishes, promotions
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(dishes.router)
     app.include_router(promotions.router)
