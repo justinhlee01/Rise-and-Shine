b61e54d74b173de393d37dee7aa4b0e06bd181d2
AndreBote <andrebote2005@gmail.com>
2025-12-01 19:39:34 -0500

    Update customers.py

--- FinalProject/api/controllers/customers.py
+++ FinalProject/api/controllers/customers.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response
-from sqlalchemy.exc import SQLAlchemyError
-
 from ..models import customers as model
+from sqlalchemy.exc import SQLAlchemyError
 
 
 def create(db: Session, request):
