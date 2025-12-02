08b3e74134596a241a447eaf32e714fab2a8104f
Patricia T <ptran23@charlotte.edu>
2025-12-01 20:45:53 -0500

    created recipes.py controller

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "rice_and_shineDB"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = "Peeled0ranges!"
     app_host = "localhost"
     app_port = 8000
