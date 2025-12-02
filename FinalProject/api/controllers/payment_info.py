9e8fd317a17c0bad37cf5dbd0746fb056f86384c
AndreBote <andrebote2005@gmail.com>
2025-12-01 20:57:07 -0500

    Update payment_info.py

--- FinalProject/api/controllers/payment_info.py
+++ FinalProject/api/controllers/payment_info.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response
-from sqlalchemy.exc import SQLAlchemyError
-
 from ..models import payment_info as model
+from sqlalchemy.exc import SQLAlchemyError
 
 
 def create(db: Session, request):
