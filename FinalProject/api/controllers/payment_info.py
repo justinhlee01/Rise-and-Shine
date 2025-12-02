c663649a4c6e1c42510f2ec1056c982ced5b195a
“Justin” <jlee356@uncc.edugit config --global user.email jlee356@uncc.edu>
2025-12-01 20:54:44 -0500

    create payment_info.py

---
+++ FinalProject/api/controllers/payment_info.py
@@ -0,0 +1,104 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from sqlalchemy.exc import SQLAlchemyError
+
+from ..models import payment_info as model
+
+
+def create(db: Session, request):
+    new_item = model.PaymentInfo(
+        order_id=request.order_id,
+        card_num=request.card_num,
+        cvv=request.cvv
+    )
+
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+
+    return new_item
+
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.PaymentInfo).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+    return result
+
+
+def read_one(db: Session, item_id: int):
+    try:
+        item = (
+            db.query(model.PaymentInfo)
+            .filter(model.PaymentInfo.payment_id == item_id)
+            .first()
+        )
+        if not item:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Id not found!"
+            )
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+    return item
+
+
+def update(db: Session, item_id: int, request):
+    try:
+        item = (
+            db.query(model.PaymentInfo)
+            .filter(model.PaymentInfo.payment_id == item_id)
+        )
+        if not item.first():
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Id not found!"
+            )
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+    return item.first()
+
+
+def delete(db: Session, item_id: int):
+    try:
+        item = (
+            db.query(model.PaymentInfo)
+            .filter(model.PaymentInfo.payment_id == item_id)
+        )
+        if not item.first():
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Id not found!"
+            )
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ FinalProject/api/routers/payment_info.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from ..controllers import payment_info as controller
+from ..schemas import payment_info as schema
+from ..dependencies.database import get_db
+
+router = APIRouter(
+    tags=["Payment Info"],
+    prefix="/payment-info"
+)
+
+
+@router.post("/", response_model=schema.Payment)
+def create(request: schema.PaymentCreate, db: Session = Depends(get_db)):
+    return controller.create(db=db, request=request)
+
+
+@router.get("/", response_model=list[schema.Payment])
+def read_all(db: Session = Depends(get_db)):
+    return controller.read_all(db)
+
+
+@router.get("/{item_id}", response_model=schema.Payment)
+def read_one(item_id: int, db: Session = Depends(get_db)):
+    return controller.read_one(db, item_id=item_id)
+
+
+@router.put("/{item_id}", response_model=schema.Payment)
+def update(item_id: int, request: schema.PaymentUpdate, db: Session = Depends(get_db)):
+    return controller.update(db=db, request=request, item_id=item_id)
+
+
+@router.delete("/{item_id}")
+def delete(item_id: int, db: Session = Depends(get_db)):
+    return controller.delete(db=db, item_id=item_id)
